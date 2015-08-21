4d7480392a4531c1209c2db49988a81a8036bffa
Pawel <brakweny@gmail.com>
2015-08-21 10:30:43 +0200

    up

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 
-function test(x) { 
-var check = Object.prototype.toString.call(x).split(' ')[1].slice(0,-1);
-return check;
-}
 
-// TODO 
-// 'cos' VS new String('cos') ...
+function toType(obj) { return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();}
+
+console.log(toType(123));
+console.log(toType('dsdsd'));
+console.log(toType(['dasdas',123]));
+console.log(toType());
+console.log(toType(true));
+console.log(toType(function(){}));
+console.log(toType(null));
+console.log(toType(/\s/g));
+
+
